c35296291ee5a0ba115486978a0e5f3d28bb377a
yup005 <yup005@ucsd.edu>
2021-07-20 00:51:17 -0700

    create table

---
+++ src/main/java/pers/pan/bankTransactions/bankDao/DockerContainer.java
@@ -0,0 +1,32 @@
+package pers.pan.bankTransactions.bankDao;
+
+import java.sql.*;
+
+
+public class DockerContainer {
+    public static void main(String[] args) {
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+        final String DB_URL = "jdbc:mysql://databasepan.cttusmxetoju.us-west-1.rds.amazonaws.com:3306/bankDB";
+        final String USER = "admin";
+        final String PASS = "password_pan";
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to a selected database...");
+            Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            System.out.println("Creating table in the database...");
+            Statement stmt = con.createStatement();
+            String sql = "CREATE TABLE TestJava "
+                    + "(AccountID INTEGER not NULL, "
+                    + "customerName VARCHAR(255), "
+                    + "email VARCHAR(255), "
+                    + "phoneNumber VARCHAR(255), "
+                    + "PRIMARY KEY ( AccountID ))";
+            stmt.executeUpdate(sql);
+            System.out.println("Successful creating!");
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
